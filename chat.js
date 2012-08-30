c1c0fcaef0fd7fd0471b94f7f61c8ce0a6bf4883
Chris Cykana <chris@cykana.com>
2012-08-30 09:24:19 -0500

    not-yet-reliable ascii animals

--- chat.js
+++ chat.js
@@ -1,27 +1,41 @@
 var _ = require('underscore')
   , net = require('net')
+  , fs = require('fs')
 
 var chat_server = net.createServer()
   , clients = [];
 
-chat_server.on('connection', function (client) {
-  var addr = client.remoteAddress
-    , port = client.remotePort;
+var critters;
 
-  client.name = '<' + addr + ':' + port + '>';
-  client.write('Hi!\n');
-  clients.push(client);
-  client.on('data', function (data) {
-    broadcast(data, client);
-  });
+fs.readFile('critters', function(err, data) {
+  if (err) throw err;
+  critters = new Buffer(data).toString('ascii').split('\n\n');
 });
 
-chat_server.listen(9000);
-
-function broadcast (message, sender) {
+function sendBroadcast (message, sender) {
   _.each(clients, function(receiver) {
     if (receiver !== sender) {
       receiver.write(sender.name + " says: " + message);
     }
   });
 }
+
+function getRandomCritter () {
+  return critters[Math.floor(Math.random() * critters.length)];
+}
+
+chat_server.on('connection', function (client) {
+  var addr = client.remoteAddress
+    , port = client.remotePort
+    , critter = getRandomCritter();
+
+  client.name = '<' + addr + ':' + port + '>';
+  client.write(critter + '\n');
+  client.write('Hi!\n');
+  clients.push(client);
+  client.on('data', function (data) {
+    sendBroadcast(data, client);
+  });
+});
+
+chat_server.listen(9000);
